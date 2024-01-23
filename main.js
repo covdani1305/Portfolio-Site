fb02c969f964b40f8ce0e345ae9bbcac36b420b7
daniel covaci <covdani1305@gmail.com>
2024-01-23 17:27:02 +0200

    test7

--- main.js
+++ main.js
@@ -1,33 +1,6 @@
-// import './style.css'
-// import javascriptLogo from './javascript.svg'
-// import viteLogo from '/vite.svg'
-// import { setupCounter } from './counter.js'
-
-// document.querySelector('#app').innerHTML = `
-//   <div>
-//     <a href="https://vitejs.dev" target="_blank">
-//       <img src="${viteLogo}" class="logo" alt="Vite logo" />
-//     </a>
-//     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-//       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-//     </a>
-//     <h1>Hello Vite!</h1>
-//     <div class="card">
-//       <button id="counter" type="button"></button>
-//     </div>
-//     <p class="read-the-docs">
-//       Click on the Vite logo to learn more
-//     </p>
-//   </div>
-// `
-
-// setupCounter(document.querySelector('#counter'))
-
 "use strict";
+import "./style.css";
 
-// reveal sections
-
-// const allSections = document.getElementsByTagName("section");
 const header = document.querySelector(".hero-section");
 
 header.classList.remove("section_hidden");
@@ -37,9 +10,9 @@ const revealSection = function (entries, observer) {
   const [entry] = entries;
   console.log(entry);
 
-  if(!entry.isIntersecting) return;
-  entry.target.classList.remove('section_hidden');
-  observer.unobserve(entry.target)
+  if (!entry.isIntersecting) return;
+  entry.target.classList.remove("section_hidden");
+  observer.unobserve(entry.target);
 };
 
 const sectionObserver = new IntersectionObserver(revealSection, {
