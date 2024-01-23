a023ce7c86984caffb3da98ff0b0ef97cf5b7639
daniel covaci <covdani1305@gmail.com>
2024-01-23 15:56:16 +0200

    implemented secion reveal on scroll animation

--- main.js
+++ main.js
@@ -23,5 +23,31 @@
 
 // setupCounter(document.querySelector('#counter'))
 
+"use strict";
 
-'use strict';
+// reveal sections
+
+// const allSections = document.getElementsByTagName("section");
+const header = document.querySelector(".hero-section");
+
+header.classList.remove("section_hidden");
+const allSections = document.querySelectorAll("section");
+
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+  console.log(entry);
+
+  if(!entry.isIntersecting) return;
+  entry.target.classList.remove('section_hidden');
+  observer.unobserve(entry.target)
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+
+allSections.forEach(function (section) {
+  section.classList.add("section_hidden");
+  sectionObserver.observe(section);
+});
