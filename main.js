2dbca1228e6a24e90c38d317c72b9fbfcd0db16d
Covaci Daniel <covdani1305@gmail.com>
2026-01-28 12:12:09 +0200

    update portfolio

--- main.js
+++ main.js
@@ -1,24 +1,24 @@
-"use strict";
-
-const header = document.querySelector(".hero-section");
-
-header.classList.remove("section_hidden");
-const allSections = document.querySelectorAll("section");
-
-const revealSection = function (entries, observer) {
-  const [entry] = entries;
-
-  if (!entry.isIntersecting) return;
-  entry.target.classList.remove("section_hidden");
-  observer.unobserve(entry.target);
-};
-
-const sectionObserver = new IntersectionObserver(revealSection, {
-  root: null,
-  threshold: 0.15,
-});
-
-allSections.forEach(function (section) {
-  section.classList.add("section_hidden");
-  sectionObserver.observe(section);
-});
+// "use strict";
+//
+// const header = document.querySelector(".hero-section");
+//
+// header.classList.remove("section_hidden");
+// const allSections = document.querySelectorAll("section");
+//
+// const revealSection = function (entries, observer) {
+//   const [entry] = entries;
+//
+//   if (!entry.isIntersecting) return;
+//   entry.target.classList.remove("section_hidden");
+//   observer.unobserve(entry.target);
+// };
+//
+// const sectionObserver = new IntersectionObserver(revealSection, {
+//   root: null,
+//   threshold: 0.15,
+// });
+//
+// allSections.forEach(function (section) {
+//   section.classList.add("section_hidden");
+//   sectionObserver.observe(section);
+// });
